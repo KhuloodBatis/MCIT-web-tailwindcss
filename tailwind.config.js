c241e5ed8ceca0e1195039aff5406ddf7d6742c3
KhuloodBatis <k.batis@bimventures.com>
2022-07-18 13:35:03 +0300

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
             colors: {
                 firstBtn: "#4364A1",
                 middBtn: "#6181B7",
+                secondBtn: "#4364A1",
                 thirdBtn: "#318EBA",
                 hoverBtn: "#25ADD7",
-                hoverBtn2: "#0888AF"
+                hoverBtn2: "#0888AF",
+                fontColor1: "#273C7B",
+                fontColor2: "#575756",
+                bgColor1: "#FAFAFA",
+                fourthBtn: "#13378E",
+
 
             },
         },
